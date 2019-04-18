9e10478365d321fe4e2d4f522b8a9ca6e3569ca0
Kerim <kerimkuscu95@gmail.com>
2019-04-18 23:23:39 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+# include "carpark.h"
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	CarPark cp;
+	cp.Initiate();
+	return 0;
+}
